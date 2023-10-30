taiyangsu/frogpilot

Numerical temperature gauge
Added function to replace the "GOOD", "OK", and "HIGH" temperature statuses with a numerical temperature gauge based on the highest temperature between the memory, CPU, and GPU by simply taping on the "Temp" gauge itself.

selfdrive/ui/qt/sidebar.cc
@@ -48,6 +48,9 @@ Sidebar::Sidebar(QWidget *parent) : QFrame(parent), onroad(false), flag_pressed(
   isStorageLeft = params.getBool("DisplayStorageLeft");
   isStorageUsed = params.getBool("DisplayStorageUsed");
 
+  isNumericalTemp = params.getBool("NumericalTemp");
+  isFahrenheit = params.getBool("Fahrenheit");
+
   isCustomTheme = params.getBool("CustomTheme");
   customColors = isCustomTheme ? params.getInt("CustomColors") : 0;
   customIcons = isCustomTheme ? params.getInt("CustomIcons") : 0;
@@ -80,8 +83,10 @@ void Sidebar::mousePressEvent(QMouseEvent *event) {
   // Declare the click boxes
   const QRect cpuRect = {30, 496, 240, 126};
   const QRect memoryRect = {30, 654, 240, 126};
+  const QRect tempRect = {30, 338, 240, 126};
   static int displayChip = 0;
   static int displayMemory = 0;
+  static int displayTemp = 0;
 
   // Swap between the respective metrics upon tap
   if (cpuRect.contains(event->pos())) {
@@ -99,6 +104,13 @@ void Sidebar::mousePressEvent(QMouseEvent *event) {
     params.putBool("DisplayMemoryUsage", isMemoryUsage);
     params.putBool("DisplayStorageLeft", isStorageLeft);
     params.putBool("DisplayStorageUsed", isStorageUsed);
+    update();
+  } else if (tempRect.contains(event->pos())) {
+    displayTemp = (displayTemp + 1) % 3;
+    isNumericalTemp = (displayTemp != 0);
+    isFahrenheit = (displayTemp == 2);
+    params.putBool("Fahrenheit", isFahrenheit);
+    params.putBool("NumericalTemp", isNumericalTemp);
     update();
   } else if (onroad && home_btn.contains(event->pos())) {
     flag_pressed = true;
@@ -152,6 +164,8 @@ void Sidebar::updateState(const UIState &s) {
   setProperty("netStrength", strength > 0 ? strength + 1 : 0);
 
   // FrogPilot properties
+  const int maxTempC = deviceState.getMaxTempC();
+  const QString max_temp = isFahrenheit ? QString::number(maxTempC * 9 / 5 + 32) + "°F" : QString::number(maxTempC) + "°C";
   const QColor theme_color = currentColors[0];
 
   // FrogPilot metrics
@@ -213,12 +227,12 @@ void Sidebar::updateState(const UIState &s) {
   }
   setProperty("connectStatus", QVariant::fromValue(connectStatus));
 
-  ItemStatus tempStatus = {{tr("TEMP"), tr("HIGH")}, danger_color};
+  ItemStatus tempStatus = {{tr("TEMP"), isNumericalTemp ? max_temp : tr("HIGH")}, danger_color};
   auto ts = deviceState.getThermalStatus();
   if (ts == cereal::DeviceState::ThermalStatus::GREEN) {
-    tempStatus = {{tr("TEMP"), tr("GOOD")}, theme_color};
+    tempStatus = {{tr("TEMP"), isNumericalTemp ? max_temp : tr("GOOD")}, theme_color};
   } else if (ts == cereal::DeviceState::ThermalStatus::YELLOW) {
-    tempStatus = {{tr("TEMP"), tr("OK")}, warning_color};
+    tempStatus = {{tr("TEMP"), isNumericalTemp ? max_temp : tr("OK")}, warning_color};
   }
   setProperty("tempStatus", QVariant::fromValue(tempStatus));
 

mike86437/openpilot

selfdrive/ui/qt/sidebar.cc
@@ -48,6 +48,9 @@ Sidebar::Sidebar(QWidget *parent) : QFrame(parent), onroad(false), flag_pressed(
   isStorageLeft = params.getBool("DisplayStorageLeft");
   isStorageUsed = params.getBool("DisplayStorageUsed");
 
+  isNumericalTemp = params.getBool("NumericalTemp");
+  isFahrenheit = params.getBool("Fahrenheit");
+
   isCustomTheme = params.getBool("CustomTheme");
   customColors = isCustomTheme ? params.getInt("CustomColors") : 0;
   customIcons = isCustomTheme ? params.getInt("CustomIcons") : 0;
@@ -80,8 +83,10 @@ void Sidebar::mousePressEvent(QMouseEvent *event) {
   // Declare the click boxes
   const QRect cpuRect = {30, 496, 240, 126};
   const QRect memoryRect = {30, 654, 240, 126};
+  const QRect tempRect = {30, 338, 240, 126};
   static int displayChip = 0;
   static int displayMemory = 0;
+  static int displayTemp = 0;
 
   // Swap between the respective metrics upon tap
   if (cpuRect.contains(event->pos())) {
@@ -99,6 +104,13 @@ void Sidebar::mousePressEvent(QMouseEvent *event) {
     params.putBoolNonBlocking("DisplayMemoryUsage", isMemoryUsage);
     params.putBoolNonBlocking("DisplayStorageLeft", isStorageLeft);
     params.putBoolNonBlocking("DisplayStorageUsed", isStorageUsed);
+    update();
+  } else if (tempRect.contains(event->pos())) {
+    displayTemp = (displayTemp + 1) % 3;
+    isNumericalTemp = (displayTemp != 0);
+    isFahrenheit = (displayTemp == 2);
+    params.putBoolNonBlocking("Fahrenheit", isFahrenheit);
+    params.putBoolNonBlocking("NumericalTemp", isNumericalTemp);
     update();
   } else if (onroad && home_btn.contains(event->pos())) {
     flag_pressed = true;
@@ -152,6 +164,8 @@ void Sidebar::updateState(const UIState &s) {
   setProperty("netStrength", strength > 0 ? strength + 1 : 0);
 
   // FrogPilot properties
+  const int maxTempC = deviceState.getMaxTempC();
+  const QString max_temp = isFahrenheit ? QString::number(maxTempC * 9 / 5 + 32) + "°F" : QString::number(maxTempC) + "°C";
   const QColor theme_color = currentColors[0];
 
   // FrogPilot metrics
@@ -213,12 +227,12 @@ void Sidebar::updateState(const UIState &s) {
   }
   setProperty("connectStatus", QVariant::fromValue(connectStatus));
 
-  ItemStatus tempStatus = {{tr("TEMP"), tr("HIGH")}, danger_color};
+  ItemStatus tempStatus = {{tr("TEMP"), isNumericalTemp ? max_temp : tr("HIGH")}, danger_color};
   auto ts = deviceState.getThermalStatus();
   if (ts == cereal::DeviceState::ThermalStatus::GREEN) {
-    tempStatus = {{tr("TEMP"), tr("GOOD")}, theme_color};
+    tempStatus = {{tr("TEMP"), isNumericalTemp ? max_temp : tr("GOOD")}, theme_color};
   } else if (ts == cereal::DeviceState::ThermalStatus::YELLOW) {
-    tempStatus = {{tr("TEMP"), tr("OK")}, warning_color};
+    tempStatus = {{tr("TEMP"), isNumericalTemp ? max_temp : tr("OK")}, warning_color};
   }
   setProperty("tempStatus", QVariant::fromValue(tempStatus));
 