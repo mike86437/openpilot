0a969f6efe9860cc09cf65097bb4b4ea04dac09c
mike8643 <nguyen95465@yahoo.com>
2023-11-11 11:39:20 -0500

    try again

--- selfdrive/navd/otisserv.py
+++ selfdrive/navd/otisserv.py
@@ -84,9 +84,22 @@ class OtisServ(BaseHTTPRequestHandler):
 
   def do_POST(self):
     postvars = self.parse_POST()
-    self.send_response(200)
-    self.send_header("Content-type", "text/html")
-    self.end_headers()
+    # Set destination endpoint
+    if self.path == '/set_destination':
+      self.send_response(200)
+      self.send_header("Content-type", "application/json")
+      self.end_headers()
+
+      # Prepare the JSON response
+      response_data = {'success': True}
+
+      # Send the JSON response
+      self.wfile.write(json.dumps(response_data).encode('utf-8'))
+    else:
+      # For non-JSON requests, proceed with your existing code
+      self.send_response(200)
+      self.send_header("Content-type", "text/html")
+      self.end_headers()
     if use_gmap:
       # gmap token
       if self.get_gmap_key() is None:
@@ -177,27 +190,10 @@ class OtisServ(BaseHTTPRequestHandler):
           else:
             self.display_page_addr_input("Place Not Found")
             return
-
-    # Set destination endpoint
-    if self.path == '/set_destination':
-      self.send_response(200)
-      self.send_header("Content-type", "application/json")
-      self.end_headers()
-
-      # Prepare the JSON response
-      response_data = {'success': True}
-
-      # Send the JSON response
-      self.wfile.write(json.dumps(response_data).encode('utf-8'))
-    else:
-      # For non-JSON requests, proceed with your existing code
-      self.send_response(200)
-      self.send_header("Content-type", "text/html")
-      self.end_headers()
-      if params.get("NavDestination") is not None:
-        self.display_nav_directions()
-      else :
-        self.display_page_addr_input()
+    if params.get("NavDestination") is not None:
+      self.display_nav_directions()
+    else :
+      self.display_page_addr_input()
 
   def get_logo(self):
     self.send_response(200)
