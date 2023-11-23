8aed96b6a0eb29fb6726891f064efe99885b7730
mike8643 <nguyen95465@yahoo.com>
2023-11-23 01:00:33 -0500

    SLC debug page

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -4,6 +4,7 @@ import numpy as np
 from openpilot.common.numpy_fast import clip, interp
 from openpilot.common.params import Params
 from cereal import log
+import json
 
 import cereal.messaging as messaging
 from openpilot.common.conversions import Conversions as CV
@@ -108,6 +109,7 @@ class LongitudinalPlanner:
     self.slc_target = 0
     self.v_offset = 0
     self.v_target = MIN_TARGET_V
+    self.testvar = 0
 
   def read_param(self):
     try:
@@ -229,6 +231,23 @@ class LongitudinalPlanner:
           v_cruise = self.slc_target
       else:
         self.slc_target = self.overridden_speed
+
+    self.testvar += 1
+    if self.testvar % 100 == 0 :
+      debug_data = {
+        "Speed_Limit": round(desired_speed_limit),
+        "override_slc": self.override_slc,
+        "overridden_speed": self.overridden_speed,
+        "v_target": self.v_target,
+        "cruise_set": v_cruise_kph * CV.KPH_TO_MS,
+        "v_cruise": v_cruise, 
+        "gasPressed": carstate.gasPressed,
+        "v_ego": v_ego
+      }
+
+      with open('debug_output.json', 'w') as json_file:
+        json.dump(debug_data, json_file, indent=2)
+        json_file.write('\n')
 
     # Pfeiferj's Vision Turn Controller
     if self.vision_turn_controller and prev_accel_constraint:
