242ed2c73e5a706a4e0d5b7cba2abb840e8b7dc7
mike8643 <nguyen95465@yahoo.com>
2023-12-01 18:37:43 -0500

    SentryDhook

--- common/params.cc
+++ common/params.cc
@@ -315,6 +315,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"SchedulePending", PERSISTENT},
     {"ScreenBrightness", PERSISTENT},
     {"SearchInput", PERSISTENT},
+    {"SentryDhook", PERSISTENT},
     {"ShowCPU", PERSISTENT},
     {"ShowFPS", PERSISTENT},
     {"ShowGPU", PERSISTENT},
