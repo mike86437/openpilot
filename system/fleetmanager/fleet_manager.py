37ff8d39b3fa54d5d23b8cb347c25f3e566dd77b
mike8643 <nguyen95465@yahoo.com>
2023-12-02 11:42:08 -0500

    ugh

--- system/fleetmanager/fleet_manager.py
+++ system/fleetmanager/fleet_manager.py
@@ -20,12 +20,8 @@ def make_request_with_retry(method, url, data=None, headers=None):
     for attempt in range(retries):
         try:
             if data is not None and method == 'POST':
-                # For POST requests, check if data is a JSON string and convert it to a dictionary
-                if isinstance(data, str):
-                    data = json.loads(data)
-
                 # Use json parameter for data
-                response = requests.request(method, url, json=data, headers=headers, stream=True)
+                response = requests.request(method, url, data=data, headers=headers, stream=True)
             else:
                 # For GET requests or other methods, use data parameter directly
                 response = requests.request(method, url, data=data, headers=headers, stream=True)
