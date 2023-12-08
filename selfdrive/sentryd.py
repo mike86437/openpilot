8aa150ca03e49a33eb4c8d303efe5a5a7abcba7f
mike8643 <98910897+mike8643@users.noreply.github.com>
2023-12-07 19:02:20 -0500

    one

--- selfdrive/sentryd.py
+++ selfdrive/sentryd.py
@@ -25,20 +25,20 @@ class SentryMode:
     self.offroad_delay = 900
 
   def takeSnapshot(self) -> Optional[Union[str, Dict[str, str]]]:
-  from openpilot.system.camerad.snapshot.snapshot import jpeg_write, snapshot
-  ret = snapshot()
-  if ret is not None:
-    def b64jpeg(x):
-      if x is not None:
-        f = io.BytesIO()
-        jpeg_write(f, x)
-        return base64.b64encode(f.getvalue()).decode("utf-8")
-      else:
-        return None
-    return {'jpegBack': b64jpeg(ret[0]),
-            'jpegFront': b64jpeg(ret[1])}
-  else:
-    raise Exception("not available while camerad is started")
+    from openpilot.system.camerad.snapshot.snapshot import jpeg_write, snapshot
+    ret = snapshot()
+    if ret is not None:
+      def b64jpeg(x):
+        if x is not None:
+          f = io.BytesIO()
+          jpeg_write(f, x)
+          return base64.b64encode(f.getvalue()).decode("utf-8")
+        else:
+          return None
+      return {'jpegBack': b64jpeg(ret[0]),
+              'jpegFront': b64jpeg(ret[1])}
+    else:
+      raise Exception("not available while camerad is started")
 
   def send_discord_webhook(self, webhook_url, message):
     data = {"content": message}
