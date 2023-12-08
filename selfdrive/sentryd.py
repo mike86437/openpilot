8473716a172acd6edce091193a478eaae3ee2c45
mike8643 <98910897+mike8643@users.noreply.github.com>
2023-12-08 01:23:17 -0500

    360

--- selfdrive/sentryd.py
+++ selfdrive/sentryd.py
@@ -9,6 +9,7 @@ import io
 import base64
 import requests
 from common.params import Params
+from PIL import Image
 
 params = Params()
 SENSITIVITY_THRESHOLD = 0.05
@@ -66,6 +67,32 @@ class SentryMode:
     dominant_axis = np.argmax(np.abs(current - previous))
     return ax_mapping[dominant_axis]
 
+  def stitch_images(front_image_path, back_image_path, output_path):
+    # Open images using PIL
+    front_image = Image.open(front_image_path)
+    back_image = Image.open(back_image_path)
+
+    # Get image sizes
+    front_width, front_height = front_image.size
+    back_width, back_height = back_image.size
+
+    # Check if images have the same height
+    if front_height != back_height:
+        print("Error: Images must have the same height.")
+        return
+
+    # Create a new image with double width
+    result_image = Image.new("RGB", (front_width + back_width, front_height))
+
+    # Paste front and back images side by side
+    result_image.paste(front_image, (0, 0))
+    result_image.paste(back_image, (front_width, 0))
+
+    # Save the stitched image
+    result_image.save(output_path)
+
+
+
   def update(self):
     t = time.monotonic()
     if not self.sentryd_init:
@@ -104,6 +131,7 @@ class SentryMode:
             json.dump(self.sentryjson, json_file, indent=4)
           message = 'ALERT! Sentry Detected Movement!'
           self.send_discord_webhook(self.webhook_url, message)
+          stitch_images('front_image.jpg', 'back_image.jpg', '360_image.jpg')
 
       # Trigger Reset
       elif self.sentry_status and time.monotonic() - self.last_timestamp > TRIGGERED_TIME:
