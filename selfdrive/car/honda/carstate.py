b66e469f71572cf6d663c55c0ee3b62502d733b4
mike8643 <nguyen95465@yahoo.com>
2023-12-15 19:45:02 -0500

    self.lkas_btn_counter = 0 self.distance_btn_counter = 0

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -105,6 +105,8 @@ class CarState(CarStateBase):
     self.brake_switch_active = False
     self.cruise_setting = 0
     self.v_cruise_pcm_prev = 0
+    self.lkas_btn_counter = 0
+    self.distance_btn_counter = 0
 
     # When available we use cp.vl["CAR_SPEED"]["ROUGH_CAR_SPEED_2"] to populate vEgoCluster
     # However, on cars without a digital speedometer this is not always present (HRV, FIT, CRV 2016, ILX and RDX)
