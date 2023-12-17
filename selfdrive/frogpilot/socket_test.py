fe77e72b97dca11aad9469486f090170c0180997
mike8643 <nguyen95465@yahoo.com>
2023-12-17 15:26:09 -0500

    main

--- selfdrive/frogpilot/socket_test.py
+++ selfdrive/frogpilot/socket_test.py
@@ -1,12 +1,16 @@
 import zmq
 import time
 
-context = zmq.Context()
-socket = context.socket(zmq.PUB)
-socket.bind("tcp://0.0.0.0:5555")
+def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind("tcp://0.0.0.0:5555")
 
-# Publish messages
-for i in range(5):
-    message = f"Hello World {i}"
-    socket.send_string(message)
-    time.sleep(1)
+    # Publish messages
+    for i in range(5):
+        message = f"Hello World {i}"
+        socket.send_string(message)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
