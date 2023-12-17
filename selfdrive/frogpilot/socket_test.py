a21aef8bae015f5f232010df1a6400e8750aa4d6
mike8643 <nguyen95465@yahoo.com>
2023-12-17 15:36:04 -0500

    indentation

--- selfdrive/frogpilot/socket_test.py
+++ selfdrive/frogpilot/socket_test.py
@@ -5,22 +5,22 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 def main():
-    context = zmq.Context()
-    socket = context.socket(zmq.PUB)
-    socket.bind("tcp://0.0.0.0:5555")
+  context = zmq.Context()
+  socket = context.socket(zmq.PUB)
+  socket.bind("tcp://0.0.0.0:5555")
 
-    try:
-        # Publish messages indefinitely
-        while True:
-            message = "Hello World"
-            socket.send_string(message)
-            time.sleep(1)
+  try:
+    # Publish messages indefinitely
+    while True:
+      message = "Hello World"
+      socket.send_string(message)
+      time.sleep(1)
 
-    except Exception as e:
-        logging.error(f"Error: {e}")
+  except Exception as e:
+    logging.error(f"Error: {e}")
 
-    finally:
-        # This block will not be reached in an infinite loop
+  finally:
+    # This block will not be reached in an infinite loop
 
 if __name__ == "__main__":
-    main()
+  main()
