d85c4360567f2c725c9eb0b1dcdae76cb6d05baf
FrogAi <91348155+FrogAi@users.noreply.github.com>
2023-12-17 14:08:42 -0700

    Model switcher
    Added model selector to swap between several different models on the fly.

--- selfdrive/ui/qt/home.cc
+++ selfdrive/ui/qt/home.cc
@@ -217,6 +217,16 @@ OffroadHome::OffroadHome(QWidget* parent) : QFrame(parent) {
       font-size: 55px;
     }
   )");
+
+  // Set the model name
+  std::map<int, QString> MODEL_NAME {
+    {0, "Blue Diamond"},
+    {1, "Farmville"},
+    {2, "New Lemon Pie"},
+    {3, "Blue Diamond V1"},
+  };
+
+  modelName = MODEL_NAME[params.getInt("Model")];
 }
 
 void OffroadHome::showEvent(QShowEvent *event) {
@@ -230,7 +240,7 @@ void OffroadHome::hideEvent(QHideEvent *event) {
 
 void OffroadHome::refresh() {
   date->setText(QLocale(uiState()->language.mid(5)).toString(QDateTime::currentDateTime(), "dddd, MMMM d"));
-  version->setText(getBrand() + " v" + getVersion().left(14).trimmed());
+  version->setText(getBrand() + " v" + getVersion().left(14).trimmed() + " - " + modelName);
 
   bool updateAvailable = update_widget->refresh();
   int alerts = alerts_widget->refresh();
