9a28ab6166f5d63520aa6b8fc48eb496204f5e80
mike8643 <nguyen95465@yahoo.com>
2023-12-17 15:29:29 -0500

    logging

--- selfdrive/frogpilot/socket_test.py
+++ selfdrive/frogpilot/socket_test.py
@@ -1,16 +1,28 @@
 import zmq
 import time
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 def main():
     context = zmq.Context()
     socket = context.socket(zmq.PUB)
     socket.bind("tcp://0.0.0.0:5555")
 
-    # Publish messages
-    for i in range(5):
-        message = f"Hello World {i}"
-        socket.send_string(message)
-        time.sleep(1)
+    try:
+        # Publish messages
+        for i in range(5):
+            message = f"Hello World {i}"
+            socket.send_string(message)
+            time.sleep(1)
+
+    except Exception as e:
+        logging.error(f"Error: {e}")
+
+    finally:
+        # Release resources
+        socket.close()
+        context.term()
 
 if __name__ == "__main__":
     main()
