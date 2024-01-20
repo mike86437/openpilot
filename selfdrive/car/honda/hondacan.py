50985c9c191f28782d2a52bc24d206f174a1aec7
mike8643 <98910897+mike8643@users.noreply.github.com>
2024-01-20 14:11:02 -0500

    revert dashed

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -156,7 +156,6 @@ def create_ui_commands(packer, CP, enabled, pcm_speed, hud, is_metric, acc_hud, 
     'SET_ME_X41': 0x41,
     'STEERING_REQUIRED': hud.steer_required,
     'SOLID_LANES': lat_active,
-    'DASHED_LANES': hud.dashed_lanes,
     'BEEP': 0,
   }
 
