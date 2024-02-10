4c5f3d2bdae772752398b17a592f1e1e2cb86eba
mike8643 <nguyen95465@yahoo.com>
2024-02-10 00:45:36 -0500

    self

--- selfdrive/frogpilot/fleetmanager/helpers.py
+++ selfdrive/frogpilot/fleetmanager/helpers.py
@@ -250,18 +250,7 @@ def nav_confirmed(postvars):
     save_type = postvars.get("save_type")
     name = postvars.get("name") if postvars.get("name") is not None else ""
     if params.get_int("SearchInput") == 1:
-      dlat = self.transform_lat(lng - 105.0, lat - 35.0)
-      dlng = self.transform_lng(lng - 105.0, lat - 35.0)
-      radlat = lat / 180.0 * pi
-      magic = math.sin(radlat)
-      magic = 1 - ee * magic * magic
-      sqrtmagic = math.sqrt(magic)
-      dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * pi)
-      dlng = (dlng * 180.0) / (a / sqrtmagic * math.cos(radlat) * pi)
-      mglat = lat + dlat
-      mglng = lng + dlng
-      lng = lng * 2 - mglng
-      lat = lat * 2 - mglat
+      lng, lat = gcj02towgs84(lng, lat)
     params.put("NavDestination", "{\"latitude\": %f, \"longitude\": %f, \"place_name\": \"%s\"}" % (lat, lng, name))
     if name == "":
       name =  str(lat) + "," + str(lng)
@@ -335,3 +324,30 @@ def amap_key_input(postvars):
     params.put("AMapKey1", token)
     params.put("AMapKey2", token2)
   return token
+
+def gcj02towgs84(lng, lat):
+  dlat = transform_lat(lng - 105.0, lat - 35.0)
+  dlng = transform_lng(lng - 105.0, lat - 35.0)
+  radlat = lat / 180.0 * pi
+  magic = math.sin(radlat)
+  magic = 1 - ee * magic * magic
+  sqrtmagic = math.sqrt(magic)
+  dlat = (dlat * 180.0) / ((a * (1 - ee)) / (magic * sqrtmagic) * pi)
+  dlng = (dlng * 180.0) / (a / sqrtmagic * math.cos(radlat) * pi)
+  mglat = lat + dlat
+  mglng = lng + dlng
+  return [lng * 2 - mglng, lat * 2 - mglat]
+
+def transform_lat(lng, lat):
+  ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + 0.1 * lng * lat + 0.2 * math.sqrt(abs(lng))
+  ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 * math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+  ret += (20.0 * math.sin(lat * pi) + 40.0 * math.sin(lat / 3.0 * pi)) * 2.0 / 3.0
+  ret += (160.0 * math.sin(lat / 12.0 * pi) + 320 * math.sin(lat * pi / 30.0)) * 2.0 / 3.0
+  return ret
+
+def transform_lng(lng, lat):
+  ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * math.sqrt(abs(lng))
+  ret += (20.0 * math.sin(6.0 * lng * pi) + 20.0 * math.sin(2.0 * lng * pi)) * 2.0 / 3.0
+  ret += (20.0 * math.sin(lng * pi) + 40.0 * math.sin(lng / 3.0 * pi)) * 2.0 / 3.0
+  ret += (150.0 * math.sin(lng / 12.0 * pi) + 300.0 * math.sin(lng / 30.0 * pi)) * 2.0 / 3.0
+  return ret
