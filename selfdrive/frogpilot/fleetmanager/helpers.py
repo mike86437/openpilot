d3020692e5da548f0b04092d36172ed127827575
mike8643 <nguyen95465@yahoo.com>
2024-02-17 15:32:15 -0500

    try single image

--- selfdrive/frogpilot/fleetmanager/helpers.py
+++ selfdrive/frogpilot/fleetmanager/helpers.py
@@ -139,10 +139,9 @@ def video_to_gif(input_path, output_path, fps=1, duration=10):
     return
   # Run ffmpeg command to convert video to gif  
   command = [
-    'ffmpeg', '-y', '-i', input_path,
-    '-filter_complex',
-    f'fps={fps},scale=240:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=32[p];[s1][p]paletteuse=dither=bayer',
-    '-t', str(duration), output_path
+    'ffmpeg', '-y', '-loop', '1', '-i', input_image,
+    '-filter_complex', f'fps={fps},scale=240:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=32[p];[s1][p]paletteuse=dither=bayer',
+    '-t', str(duration), output_gif
   ]
   subprocess.run(command)
   print(f"GIF file created: {output_path}")
