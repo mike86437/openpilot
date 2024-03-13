811bcb12f2dce69d762cfc2bb374a6731bcbf936
mike8643 <nguyen95465@yahoo.com>
2024-03-12 23:33:54 -0400

    checks

--- selfdrive/frogpilot/install_zerotier.sh
+++ selfdrive/frogpilot/install_zerotier.sh
@@ -4,12 +4,26 @@ function install_debug_softwares {
   sudo mount -o remount,rw /
   curl -s https://install.zerotier.com | sudo bash
   sudo systemctl stop zerotier-one
-  sudo mkdir -p /data/zerotier/
+  
+  # Check if the directory already exists
+  if [ ! -d "/persist/zerotier" ]; then
+    # If the directory doesn't exist, create it
+    sudo mkdir -p /persist/zerotier/
+  fi
+
   sudo mount -o remount,rw /
-  sudo sed -i 's/\/usr\/sbin\/zerotier-one/\/usr\/sbin\/zerotier-one \/data\/zerotier\//g' /lib/systemd/system/zerotier-one.service
+  
+  # Check if the modification already exists in the file
+  if ! sudo grep -q '/usr/sbin/zerotier-one -d"/persist/zerotier/"' /lib/systemd/system/zerotier-one.service; then
+    # If the modification doesn't exist, apply it
+    sudo sed -i 's|ExecStart=/usr/sbin/zerotier-one|ExecStart=/usr/sbin/zerotier-one -d"/persist/zerotier/"|' /lib/systemd/system/zerotier-one.service
+  fi
+
   sudo systemctl daemon-reload
   sudo systemctl start zerotier-one
-  sudo zerotier-cli -D"/data/zerotier" join "$(cat /data/params/d/ZeroTier)"
+
+  # Proceed with joining ZeroTier network
+  sudo zerotier-cli -D"/persist/zerotier" join "$(</data/params/d/ZeroTier)"
 }
 
 install_debug_softwares
