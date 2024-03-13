3bec969da11527cde4bdeefc7a085d9be730c66c
mike8643 <nguyen95465@yahoo.com>
2024-03-13 18:57:00 -0400

    try to get model prob

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -1075,16 +1075,18 @@ void AnnotatedCameraWidget::drawLead(QPainter &painter, const cereal::RadarState
   if (leadInfo) {
     // Declare the variables
     float lead_speed = std::max(lead_data.getVLead(), 0.0f);  // Ensure lead speed doesn't go under 0 m/s cause that's dumb
+    float model_prob = std::max(lead_data.getModelProb(), 0.0f);
 
     // Form the text and center it below the chevron
     painter.setPen(Qt::white);
     painter.setFont(InterFont(35, QFont::Bold));
 
-    QString text = QString("%1 %2 | %3 %4")
+    QString text = QString("%1 %2 | %3 %4 | %5%%")
                            .arg(qRound(d_rel * distanceConversion))
                            .arg(leadDistanceUnit)
                            .arg(qRound(lead_speed * speedConversion))
-                           .arg(leadSpeedUnit);
+                           .arg(leadSpeedUnit)
+                           .arg(model_prob, 0, 'f', 2, '0');
 
     // Calculate the text starting position
     QFontMetrics metrics(painter.font());
