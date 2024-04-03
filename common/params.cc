4803f81496f2647aec86b3eed8546b7846fe05e3
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-04-02 23:25:13 -0700

    Lead departing alert
    Added toggle to enable an alert when the lead vehicle starts to depart.

--- common/params.cc
+++ common/params.cc
@@ -291,6 +291,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"IncreaseThermalLimits", PERSISTENT},
     {"LaneLinesWidth", PERSISTENT},
     {"LateralTune", PERSISTENT},
+    {"LeadDepartingAlert", PERSISTENT},
     {"LeadInfo", PERSISTENT},
     {"LongitudinalTune", PERSISTENT},
     {"LongPitch", PERSISTENT},
