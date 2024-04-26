https-githob-com-Lotx-001/HPILOT

Speed limit controller
Added toggle to control the cruise set speed according to speed limit supplied by OSM, NOO, or the vehicle itself.

Co-Authored-By: Jacob Pfeifer <jacob@pfeifer.dev>
Co-Authored-By: Efini <19368997+efini@users.noreply.github.com>
Co-Authored-By: Kumar <36933347+rav4kumar@users.noreply.github.com>
Co-Authored-By: multikyd <72329880+multikyd@users.noreply.github.com>

common/params.cc
@@ -231,6 +231,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"CameraView", PERSISTENT},
     {"CarMake", PERSISTENT},
     {"CarModel", PERSISTENT},
+    {"CarSpeedLimit", PERSISTENT},
     {"CECurves", PERSISTENT},
     {"CECurvesLead", PERSISTENT},
     {"CENavigation", PERSISTENT},
@@ -278,6 +279,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"FingerprintLogged", CLEAR_ON_MANAGER_START},
     {"ForceAutoTune", PERSISTENT},
     {"ForceFingerprint", PERSISTENT},
+    {"ForceMPHDashboard", PERSISTENT},
     {"FPSCounter", PERSISTENT},
     {"FrogPilotTogglesUpdated", PERSISTENT},
     {"FrogsGoMoo", PERSISTENT},
@@ -316,6 +318,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"MapboxPublicKey", PERSISTENT},
     {"MapboxSecretKey", PERSISTENT},
     {"MapsSelected", PERSISTENT},
+    {"MapSpeedLimit", PERSISTENT},
     {"MapTargetLatA", PERSISTENT},
     {"MapTargetVelocities", PERSISTENT},
     {"Model", PERSISTENT},
@@ -328,6 +331,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"MTSCCurvatureCheck", PERSISTENT},
     {"MTSCEnabled", PERSISTENT},
     {"MuteOverheated", PERSISTENT},
+    {"NavSpeedLimit", PERSISTENT},
     {"NNFF", PERSISTENT},
     {"NNFFLite", PERSISTENT},
     {"NNFFModelName", PERSISTENT},
@@ -336,6 +340,10 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"NudgelessLaneChange", PERSISTENT},
     {"NumericalTemp", PERSISTENT},
     {"OfflineMode", PERSISTENT},
+    {"Offset1", PERSISTENT},
+    {"Offset2", PERSISTENT},
+    {"Offset3", PERSISTENT},
+    {"Offset4", PERSISTENT},
     {"OneLaneChange", PERSISTENT},
     {"OnroadDistanceButton", PERSISTENT},
     {"OnroadDistanceButtonPressed", PERSISTENT},
@@ -348,6 +356,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"PauseLateralSpeed", PERSISTENT},
     {"PedalsOnUI", PERSISTENT},
     {"PreferredSchedule", PERSISTENT},
+    {"PreviousSpeedLimit", PERSISTENT},
     {"PromptDistractedVolume", PERSISTENT},
     {"PromptVolume", PERSISTENT},
     {"QOLControls", PERSISTENT},
@@ -369,15 +378,32 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"ScreenTimeout", PERSISTENT},
     {"ScreenTimeoutOnroad", PERSISTENT},
     {"SearchInput", PERSISTENT},
+    {"SetSpeedLimit", PERSISTENT},
     {"SetSpeedOffset", PERSISTENT},
     {"ShowCPU", PERSISTENT},
     {"ShowGPU", PERSISTENT},
     {"ShowIP", PERSISTENT},
     {"ShowMemoryUsage", PERSISTENT},
+    {"ShowSLCOffset", PERSISTENT},
+    {"ShowSLCOffsetUI", PERSISTENT},
     {"ShowStorageLeft", PERSISTENT},
     {"ShowStorageUsed", PERSISTENT},
     {"Sidebar", PERSISTENT},
+    {"SLCConfirmation", PERSISTENT},
+    {"SLCConfirmationLower", PERSISTENT},
+    {"SLCConfirmationHigher", PERSISTENT},
+    {"SLCConfirmed", PERSISTENT},
+    {"SLCConfirmedPressed", PERSISTENT},
+    {"SLCLookahead", PERSISTENT},
+    {"SLCFallback", PERSISTENT},
+    {"SLCOverride", PERSISTENT},
+    {"SLCPriority", PERSISTENT},
+    {"SLCPriority1", PERSISTENT},
+    {"SLCPriority2", PERSISTENT},
+    {"SLCPriority3", PERSISTENT},
     {"SmoothBraking", PERSISTENT},
+    {"SpeedLimitController", PERSISTENT},
+    {"SpeedLimitChangedAlert", PERSISTENT},
     {"StandardFollow", PERSISTENT},
     {"StandardJerk", PERSISTENT},
     {"StandbyMode", PERSISTENT},
@@ -387,6 +413,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"UnlimitedLength", PERSISTENT},
     {"Updated", PERSISTENT},
     {"UseSI", PERSISTENT},
+    {"UseVienna", PERSISTENT},
     {"WarningImmediateVolume", PERSISTENT},
     {"WarningSoftVolume", PERSISTENT},
 };

mike86437/openpilot

common/params.cc
@@ -231,6 +231,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"CameraView", PERSISTENT},
     {"CarMake", PERSISTENT},
     {"CarModel", PERSISTENT},
+    {"CarSpeedLimit", PERSISTENT},
     {"CECurves", PERSISTENT},
     {"CECurvesLead", PERSISTENT},
     {"CENavigation", PERSISTENT},
@@ -280,6 +281,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"FingerprintLogged", CLEAR_ON_MANAGER_START},
     {"ForceAutoTune", PERSISTENT},
     {"ForceFingerprint", PERSISTENT},
+    {"ForceMPHDashboard", PERSISTENT},
     {"FPSCounter", PERSISTENT},
     {"FrogPilotTogglesUpdated", PERSISTENT},
     {"FrogsGoMoo", PERSISTENT},
@@ -319,6 +321,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"MapboxPublicKey", PERSISTENT},
     {"MapboxSecretKey", PERSISTENT},
     {"MapsSelected", PERSISTENT},
+    {"MapSpeedLimit", PERSISTENT},
     {"MapTargetLatA", PERSISTENT},
     {"MapTargetVelocities", PERSISTENT},
     {"Model", PERSISTENT},
@@ -330,6 +333,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"MTSCAggressiveness", PERSISTENT},
     {"MTSCCurvatureCheck", PERSISTENT},
     {"MTSCEnabled", PERSISTENT},
+    {"NavSpeedLimit", PERSISTENT},
     {"NNFF", PERSISTENT},
     {"NNFFLite", PERSISTENT},
     {"NNFFModelName", PERSISTENT},
@@ -338,6 +342,10 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"NudgelessLaneChange", PERSISTENT},
     {"NumericalTemp", PERSISTENT},
     {"OfflineMode", PERSISTENT},
+    {"Offset1", PERSISTENT},
+    {"Offset2", PERSISTENT},
+    {"Offset3", PERSISTENT},
+    {"Offset4", PERSISTENT},
     {"OneLaneChange", PERSISTENT},
     {"OnroadDistanceButton", PERSISTENT},
     {"OnroadDistanceButtonPressed", PERSISTENT},
@@ -351,6 +359,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"PauseLateralSpeed", PERSISTENT},
     {"PedalsOnUI", PERSISTENT},
     {"PreferredSchedule", PERSISTENT},
+    {"PreviousSpeedLimit", PERSISTENT},
     {"PromptDistractedVolume", PERSISTENT},
     {"PromptVolume", PERSISTENT},
     {"QOLControls", PERSISTENT},
@@ -372,19 +381,37 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"ScreenTimeout", PERSISTENT},
     {"ScreenTimeoutOnroad", PERSISTENT},
     {"SearchInput", PERSISTENT},
+    {"SetSpeedLimit", PERSISTENT},
     {"SetSpeedOffset", PERSISTENT},
     {"ShowCPU", PERSISTENT},
     {"ShowGPU", PERSISTENT},
     {"ShowIP", PERSISTENT},
     {"ShowJerk", PERSISTENT},
     {"ShowMemoryUsage", PERSISTENT},
+    {"ShowSLCOffset", PERSISTENT},
+    {"ShowSLCOffsetUI", PERSISTENT},
     {"ShowStorageLeft", PERSISTENT},
     {"ShowStorageUsed", PERSISTENT},
     {"ShowTuning", PERSISTENT},
     {"Sidebar", PERSISTENT},
+    {"SLCConfirmation", PERSISTENT},
+    {"SLCConfirmationLower", PERSISTENT},
+    {"SLCConfirmationHigher", PERSISTENT},
+    {"SLCConfirmed", PERSISTENT},
+    {"SLCConfirmedPressed", PERSISTENT},
+    {"SLCLookaheadHigher", PERSISTENT},
+    {"SLCLookaheadLower", PERSISTENT},
+    {"SLCFallback", PERSISTENT},
+    {"SLCOverride", PERSISTENT},
+    {"SLCPriority", PERSISTENT},
+    {"SLCPriority1", PERSISTENT},
+    {"SLCPriority2", PERSISTENT},
+    {"SLCPriority3", PERSISTENT},
     {"SmoothBraking", PERSISTENT},
     {"SmoothBrakingFarLead", PERSISTENT},
     {"SmoothBrakingJerk", PERSISTENT},
+    {"SpeedLimitController", PERSISTENT},
+    {"SpeedLimitChangedAlert", PERSISTENT},
     {"StandardFollow", PERSISTENT},
     {"StandardJerk", PERSISTENT},
     {"StandbyMode", PERSISTENT},
@@ -396,6 +423,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"UnlockDoors", PERSISTENT},
     {"Updated", PERSISTENT},
     {"UseSI", PERSISTENT},
+    {"UseVienna", PERSISTENT},
     {"WarningImmediateVolume", PERSISTENT},
     {"WarningSoftVolume", PERSISTENT},
 };