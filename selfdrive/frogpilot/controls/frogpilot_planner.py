580b6e704977ca4d0055d593792cc41b961c3d99
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-24 02:43:03 -0700

    Controls - Longitudinal Tuning - Deceleration Profile
    Change the deceleration rate to be either sporty or eco-friendly.

--- selfdrive/frogpilot/controls/frogpilot_planner.py
+++ selfdrive/frogpilot/controls/frogpilot_planner.py
@@ -20,15 +20,25 @@ from openpilot.selfdrive.frogpilot.controls.lib.frogpilot_variables import CITY_
 
 GearShifter = car.CarState.GearShifter
 
+                 # MPH = [0., 18,  36,  63,  94]
+A_CRUISE_MIN_BP_CUSTOM = [0., 8., 16., 28., 42.]
                  # MPH = [0., 6.71, 13.4, 17.9, 24.6, 33.6, 44.7, 55.9, 89.5]
 A_CRUISE_MAX_BP_CUSTOM = [0.,    3,   6.,   8.,  11.,  15.,  20.,  25.,  40.]
 
+A_CRUISE_MIN_VALS_ECO = [-0.001, -0.010, -0.28, -0.56, -0.56]
 A_CRUISE_MAX_VALS_ECO = [3.5, 3.2, 2.3, 2.0, 1.15, .80, .58, .36, .30]
 
+A_CRUISE_MIN_VALS_SPORT = [-0.50, -0.52, -0.55, -0.57, -0.60]
 A_CRUISE_MAX_VALS_SPORT = [3.5, 3.5, 3.3, 2.8, 1.5, 1.0, 0.75, 0.65, 0.6]
+
+def get_min_accel_eco(v_ego):
+  return interp(v_ego, A_CRUISE_MIN_BP_CUSTOM, A_CRUISE_MIN_VALS_ECO)
 
 def get_max_accel_eco(v_ego):
   return interp(v_ego, A_CRUISE_MAX_BP_CUSTOM, A_CRUISE_MAX_VALS_ECO)
+
+def get_min_accel_sport(v_ego):
+  return interp(v_ego, A_CRUISE_MIN_BP_CUSTOM, A_CRUISE_MIN_VALS_SPORT)
 
 def get_max_accel_sport(v_ego):
   return interp(v_ego, A_CRUISE_MAX_BP_CUSTOM, A_CRUISE_MAX_VALS_SPORT)
@@ -71,6 +81,10 @@ class FrogPilotPlanner:
 
     if controlsState.experimentalMode:
       self.min_accel = ACCEL_MIN
+    elif frogpilot_toggles.deceleration_profile == 1:
+      self.min_accel = get_min_accel_eco(v_ego)
+    elif frogpilot_toggles.deceleration_profile == 2:
+      self.min_accel = get_min_accel_sport(v_ego)
     else:
       self.min_accel = A_CRUISE_MIN
 
