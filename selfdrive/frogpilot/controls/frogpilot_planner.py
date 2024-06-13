35588795d234f9a2ea49c6d6595ebce9c88dd925
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-13 16:51:02 -0700

    Controls - Longitudinal Tuning - Traffic Mode
    Enable the ability to activate 'Traffic Mode' by holding down the 'distance' button for 2.5 seconds. When 'Traffic Mode' is active the onroad UI will turn red and openpilot will drive catered towards stop and go traffic.

--- selfdrive/frogpilot/controls/frogpilot_planner.py
+++ selfdrive/frogpilot/controls/frogpilot_planner.py
@@ -31,6 +31,8 @@ A_CRUISE_MAX_VALS_ECO = [3.5, 3.2, 2.3, 2.0, 1.15, .80, .58, .36, .30]
 A_CRUISE_MIN_VALS_SPORT = [-0.50, -0.52, -0.55, -0.57, -0.60]
 A_CRUISE_MAX_VALS_SPORT = [3.5, 3.5, 3.3, 2.8, 1.5, 1.0, 0.75, 0.65, 0.6]
 
+TRAFFIC_MODE_BP = [0., CITY_SPEED_LIMIT]
+
 def get_min_accel_eco(v_ego):
   return interp(v_ego, A_CRUISE_MIN_BP_CUSTOM, A_CRUISE_MIN_VALS_ECO)
 
@@ -67,7 +69,7 @@ class FrogPilotPlanner:
     v_ego = max(carState.vEgo, 0)
     v_lead = self.lead_one.vLead
 
-    distance_offset = max(frogpilot_toggles.increased_stopping_distance + min(CITY_SPEED_LIMIT - v_ego, 0), 0)
+    distance_offset = max(frogpilot_toggles.increased_stopping_distance + min(CITY_SPEED_LIMIT - v_ego, 0), 0) if not frogpilotCarState.trafficModeActive else 0
     lead_distance = self.lead_one.dRel - distance_offset
     stopping_distance = STOP_DISTANCE + distance_offset
 
@@ -89,13 +91,18 @@ class FrogPilotPlanner:
     else:
       self.min_accel = A_CRUISE_MIN
 
-    self.base_acceleration_jerk, self.base_speed_jerk = get_jerk_factor(
-      frogpilot_toggles.custom_personalities, frogpilot_toggles.aggressive_jerk_acceleration, frogpilot_toggles.aggressive_jerk_speed,
-      frogpilot_toggles.standard_jerk_acceleration, frogpilot_toggles.standard_jerk_speed, frogpilot_toggles.relaxed_jerk_acceleration,
-      frogpilot_toggles.relaxed_jerk_speed, controlsState.personality)
-    self.t_follow = get_T_FOLLOW(
-      frogpilot_toggles.custom_personalities, frogpilot_toggles.aggressive_follow, frogpilot_toggles.standard_follow,
-      frogpilot_toggles.relaxed_follow, controlsState.personality)
+    if frogpilotCarState.trafficModeActive:
+      self.base_acceleration_jerk = interp(v_ego, TRAFFIC_MODE_BP, frogpilot_toggles.traffic_mode_jerk_acceleration)
+      self.base_speed_jerk = interp(v_ego, TRAFFIC_MODE_BP, frogpilot_toggles.traffic_mode_jerk_speed)
+      self.t_follow = interp(v_ego, TRAFFIC_MODE_BP, frogpilot_toggles.traffic_mode_t_follow)
+    else:
+      self.base_acceleration_jerk, self.base_speed_jerk = get_jerk_factor(
+        frogpilot_toggles.custom_personalities, frogpilot_toggles.aggressive_jerk_acceleration, frogpilot_toggles.aggressive_jerk_speed,
+        frogpilot_toggles.standard_jerk_acceleration, frogpilot_toggles.standard_jerk_speed, frogpilot_toggles.relaxed_jerk_acceleration,
+        frogpilot_toggles.relaxed_jerk_speed, controlsState.personality)
+      self.t_follow = get_T_FOLLOW(
+        frogpilot_toggles.custom_personalities, frogpilot_toggles.aggressive_follow, frogpilot_toggles.standard_follow,
+        frogpilot_toggles.relaxed_follow, controlsState.personality)
 
     if self.tracking_lead and driving_gear:
       self.update_follow_values(lead_distance, stopping_distance, v_ego, v_lead, frogpilot_toggles)
