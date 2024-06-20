2ffe661bf71871f73d351cb133f74274dc365b17
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-06-19 17:04:09 -0700

    Controls - Conditional Experimental Mode - Curve Detected Ahead
    Switch to 'Experimental Mode' when a curve is detected.

--- selfdrive/frogpilot/controls/lib/conditional_experimental_mode.py
+++ selfdrive/frogpilot/controls/lib/conditional_experimental_mode.py
@@ -7,7 +7,10 @@ class ConditionalExperimentalMode:
   def __init__(self):
     self.params_memory = Params("/dev/shm/params")
 
+    self.curve_detected = False
     self.experimental_mode = False
+
+    self.curvature_mac = MovingAverageCalculator()
 
   def update(self, carState, enabled, frogpilotNavigation, lead, modelData, road_curvature, slower_lead, v_ego, v_lead, frogpilot_toggles):
     self.update_conditions(lead.dRel, lead.status, modelData, road_curvature, slower_lead, v_ego, v_lead, frogpilot_toggles)
@@ -22,6 +25,18 @@ class ConditionalExperimentalMode:
       self.status_value = 7 if lead_status else 8
       return True
 
+    if frogpilot_toggles.conditional_curves and self.curve_detected:
+      self.status_value = 13
+      return True
+
     return False
 
   def update_conditions(self, lead_distance, lead_status, modelData, road_curvature, slower_lead, v_ego, v_lead, frogpilot_toggles):
+    self.road_curvature(lead_status, road_curvature, v_ego, frogpilot_toggles)
+
+  def road_curvature(self, lead_status, road_curvature, v_ego, frogpilot_toggles):
+    curve_detected = (1 / road_curvature)**0.5 < v_ego and (frogpilot_toggles.conditional_curves_lead or not lead_status)
+    curve_active = (0.9 / road_curvature)**0.5 < v_ego and self.curve_detected
+
+    self.curvature_mac.add_data(curve_detected or curve_active)
+    self.curve_detected = self.curvature_mac.get_moving_average() >= PROBABILITY
