eb3f29c8df5328c4007336ae4e636d8dc53cfc25
mike8643 <nguyen95465@yahoo.com>
2024-06-22 12:05:07 -0400

    lat long drive stats

--- selfdrive/ui/qt/widgets/drive_stats.h
+++ selfdrive/ui/qt/widgets/drive_stats.h
@@ -22,7 +22,7 @@ private:
   QJsonDocument stats_;
   struct StatsLabels {
     QLabel *routes, *distance, *distance_unit, *hours;
-  } all_, week_, frogPilot_;
+  } frogPilot_, frogLat_, frogLong_;
 
 private slots:
   void parseResponse(const QString &response, bool success);
