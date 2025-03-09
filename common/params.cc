9e2452b55a12ff2b1e264899dbfbed966d01cdcf
mike8643 <nguyen95465@yahoo.com>
2025-03-08 19:04:18 -0500

    SentryD

--- common/params.cc
+++ common/params.cc
@@ -445,6 +445,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"ScreenTimeout", PERSISTENT},
     {"ScreenTimeoutOnroad", PERSISTENT},
     {"SearchInput", PERSISTENT},
+    {"SentryDhook", PERSISTENT},
     {"SetSpeedLimit", PERSISTENT},
     {"SetSpeedOffset", PERSISTENT},
     {"ShowCEMStatus", PERSISTENT},
---
+++ selfdrive/frogpilot/sentryd.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import numpy as np
+import cereal.messaging as messaging
+import time
+from openpilot.selfdrive.controls.lib.events import Events
+import requests
+from common.params import Params
+params = Params()
+SENSITIVITY_THRESHOLD = 0.1
+TRIGGERED_TIME = 2
+# Credit to RoyJr
+
+class SentryMode:
+
+  def __init__(self):
+    self.sm = messaging.SubMaster(['accelerometer'])
+    self.curr_accel = 0
+    self.prev_accel = None
+    self.sentry_status = False
+    self.events = Events()
+    self.secDelay = 0
+    self.webhook_url = params.get("SentryDhook", encoding='utf8')
+
+  def send_discord_webhook(self, webhook_url, message):
+    data = {"content": message}
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(webhook_url, json=data, headers=headers)
+    if response.status_code == 200:
+      print("Message sent successfully")
+    else:
+      print(f"Failed to send message. Status code: {response.status_code}")
+
+  def get_movement_type(self, current, previous):
+    ax_mapping = {0: "X", 1: "Y", 2: "Z"}
+    dominant_axis = np.argmax(np.abs(current - previous))
+    return ax_mapping[dominant_axis]
+
+  def update(self):
+    events = Events()
+    # Extract acceleration data
+    self.curr_accel = np.array(self.sm['accelerometer'].acceleration.v)
+
+    # Initialize
+    if self.prev_accel is None:
+      self.prev_accel = self.curr_accel
+
+    # Calculate magnitude change
+    delta = abs(np.linalg.norm(self.curr_accel) - np.linalg.norm(self.prev_accel))
+
+    # Trigger Check
+    if delta > SENSITIVITY_THRESHOLD:
+      self.last_timestamp = time.monotonic()
+      self.sentry_status = True
+      self.secDelay += 1
+      if self.secDelay % 100 == 0 and self.webhook_url is not None:
+        message = 'ALERT! Sentry Detected Movement!'
+        self.send_discord_webhook(self.webhook_url, message)
+
+    # Trigger Reset
+    elif self.sentry_status and time.monotonic() - self.last_timestamp > TRIGGERED_TIME:
+      self.sentry_status = False
+      print("Movement Ended")
+
+    self.prev_accel = self.curr_accel
+
+  def start(self):
+    while True:
+      self.sm.update()
+      self.update()
+
+def main():
+  sentry_mode = SentryMode()
+  sentry_mode.start()
+
+
+if __name__ == "__main__":
+  main()
