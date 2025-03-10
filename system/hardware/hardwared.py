1d0521470b35c4b8bf9cad0de5e76bc235bb8fe8
mike8643 <nguyen95465@yahoo.com>
2025-03-10 00:41:24 -0400

    while params.get_bool("IsTakingSnapshot"):

--- system/hardware/hardwared.py
+++ system/hardware/hardwared.py
@@ -309,6 +309,9 @@ def hardware_thread(end_event, hw_queue) -> None:
     startup_conditions["free_space"] = msg.deviceState.freeSpacePercent > 2
     startup_conditions["completed_training"] = params.get("CompletedTrainingVersion") == training_version
     startup_conditions["not_driver_view"] = not params.get_bool("IsDriverViewEnabled")
+
+    while params.get_bool("IsTakingSnapshot"):
+      time.sleep(0.5)
     startup_conditions["not_taking_snapshot"] = not params.get_bool("IsTakingSnapshot")
 
     # must be at an engageable thermal band to go onroad
