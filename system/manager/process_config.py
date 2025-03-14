9a8380ef0e61f9463ae6e9dd2d178d1d9c5b6414
mike8643 <nguyen95465@yahoo.com>
2025-03-14 10:01:17 -0400

    pond testing

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -111,6 +111,7 @@ procs = [
   PythonProcess("frogpilot_process", "selfdrive.frogpilot.frogpilot_process", always_run),
   PythonProcess("mapd", "selfdrive.frogpilot.navigation.mapd", always_run),
   PythonProcess("sentryd", "selfdrive.frogpilot.sentryd", only_offroad),
+  PythonProcess("thepond", "selfdrive.frogpilot.thepond.thepond", always_run),
   NativeProcess("tinygrad_modeld", "selfdrive/tinygrad_modeld", ["./tinygrad_modeld"], run_tinygrad_modeld),
 ]
 
