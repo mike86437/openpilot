760e26875c2d2f06dd71698699af47149d48a99c
mike8643 <nguyen95465@yahoo.com>
2025-03-13 22:02:55 -0400

    hazzah, remove debug directions

--- selfdrive/frogpilot/fleetmanager/helpers.py
+++ selfdrive/frogpilot/fleetmanager/helpers.py
@@ -225,7 +225,6 @@ def ffplay_mp4_wrap_process_builder(file_name):
 
 def get_nav_active():
   if params.get("NavDestination", encoding='utf8') is not None:
-    print("Debug Directions")
     return True
   else:
     return False
