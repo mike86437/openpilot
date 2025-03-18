c48274ea4483342f2401d121aac0ea9bdc1e1984
mike8643 <nguyen95465@yahoo.com>
2025-03-18 08:16:14 -0400

    0.35P 1.928 constant

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -82,7 +82,7 @@ class CarInterface(CarInterfaceBase):
     else:
       # default longitudinal tuning for all hondas
       ret.longitudinalTuning.kiBP = [0., 5., 35.]
-      ret.longitudinalTuning.kiV = [1.2, 0.8, 0.5]
+      ret.longitudinalTuning.kiV = [1.2, 0.35, 0.35]
 
     eps_modified = False
     for fw in car_fw:
