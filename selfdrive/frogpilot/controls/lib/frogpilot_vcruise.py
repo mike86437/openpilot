3cf11eb0c516be17db9202a9baa60bc236684af5
mike8643 <nguyen95465@yahoo.com>
2025-04-02 17:16:10 -0400

    Float 5 mph over/under vcruise

--- selfdrive/frogpilot/controls/lib/frogpilot_vcruise.py
+++ selfdrive/frogpilot/controls/lib/frogpilot_vcruise.py
@@ -132,6 +132,14 @@ class FrogPilotVCruise:
     else:
       self.vtsc_target = v_cruise if v_cruise != V_CRUISE_UNSET else 0
 
+    # Float 5 mph over vcruise
+    if self.vtsc_target >= v_cruise and self.mtsc_target >= v_cruise and v_ego > (v_cruise + 0.5):
+      v_cruise = min(v_ego - 0.5, v_cruise + 2.2352)
+
+    # Float 5 mph under vcruise
+    if self.vtsc_target >= v_cruise and self.mtsc_target >= v_cruise and v_ego < (v_cruise - 0.5):
+      v_cruise = max(v_ego + 0.5, v_cruise - 2.2352)
+
     if frogpilot_toggles.force_standstill and carState.standstill and not self.override_force_stop and controlsState.enabled and not (self.frogpilot_planner.tracking_lead and getattr(lead, "dRel", float("inf")) < 15):
       self.forcing_stop = True
       v_cruise = -1
