3ee5cec7382707d228f928a35ed18182a1a7e4f9
mike8643 <nguyen95465@yahoo.com>
2025-04-04 18:16:37 -0400

    #

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -337,7 +337,7 @@ class Controls:
     num_events = len(self.events)
 
     not_running = {p.name for p in self.sm['managerState'].processes if not p.running and p.shouldBeRunning}
-    if False: self.sm.recv_frame['managerState'] and (not_running - IGNORE_PROCESSES):
+    if False: # self.sm.recv_frame['managerState'] and (not_running - IGNORE_PROCESSES):
       self.events.add(EventName.processNotRunning)
       if not_running != self.not_running_prev:
         cloudlog.event("process_not_running", not_running=not_running, error=True)
