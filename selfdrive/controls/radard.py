2c559d454e49352c87619d10355d15194c023384
mike8643 <nguyen95465@yahoo.com>
2025-04-09 16:45:12 -0400

    :

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -123,7 +123,7 @@ class Track:
     left_lane = interp(self.dRel, model_data.laneLines[1].x, model_data.laneLines[1].y)
     right_lane = interp(self.dRel, model_data.laneLines[2].x, model_data.laneLines[2].y)
 
-    if left_lane < self.yRel < right_lane
+    if left_lane < self.yRel < right_lane:
       print(f"Left: {left_lane:.2f}, Y Rel: {self.yRel:.2f}, Right: {right_lane:.2f}, Dist: {self.dRel:.2f}")
 
     return left_lane < self.yRel < right_lane
