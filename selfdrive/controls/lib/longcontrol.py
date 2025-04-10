78fc0691e24077bc2d59167c08039eeb26a2ff8b
mike8643 <98910897+mike8643@users.noreply.github.com>
2025-04-10 11:15:29 -0400

    Update longcontrol.py

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -123,7 +123,7 @@ class LongControl:
       output_accel = (a_target if frogpilot_toggles.human_acceleration else self.CP.startAccel)
       self.reset()
 
-    elif 0 < leadOne.dRel < 100 and CS.cruiseState.speed < CS.vEgo:
+    elif 0 < leadOne.dRel < 100 and CS.cruiseState.speed > CS.vEgo:
       dFollow = t_follow * CS.vEgo + 10.0
       self.dRelk = 0.8 * float(leadOne.dRel) + 0.2 * self.dRelk
       error = (self.dRelk - dFollow) / 10.0
