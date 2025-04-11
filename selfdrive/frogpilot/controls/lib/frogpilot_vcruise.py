91cac8b8d0b6892eba768ba1ae44c4381952d0a1
mike8643 <nguyen95465@yahoo.com>
2025-04-11 09:13:46 -0400

    add back setcoast

--- selfdrive/frogpilot/controls/lib/frogpilot_vcruise.py
+++ selfdrive/frogpilot/controls/lib/frogpilot_vcruise.py
@@ -85,6 +85,8 @@ class FrogPilotVCruise:
         trim = min(trim, max_trim)
         trimmed_vego = v_ego - max(0.0, trim)
         if self.mtsc_target > trimmed_vego: self.mtsc_target = trimmed_vego
+      elif self.params.get_bool("SetCoast"):
+        self.mtsc_target = max(v_ego - 2, CRUISING_SPEED)
       else:
         self.mtsc_target = v_cruise if v_cruise != V_CRUISE_UNSET else 0
         mtsc_active = False
