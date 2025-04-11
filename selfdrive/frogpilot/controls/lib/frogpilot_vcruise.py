50e0de2355204c15bde6c09d48d452f88facc56c
mike8643 <98910897+mike8643@users.noreply.github.com>
2025-04-10 22:40:23 -0400

    Update frogpilot_vcruise.py

--- selfdrive/frogpilot/controls/lib/frogpilot_vcruise.py
+++ selfdrive/frogpilot/controls/lib/frogpilot_vcruise.py
@@ -76,7 +76,7 @@ class FrogPilotVCruise:
       # trim v_ego to when closer than expected following distance
       self.dRelk = 0.8 * float(lead.dRel) + 0.2 * self.dRelk
       self.vRelk = 0.8 * float(v_rel) + 0.2 * self.vRelk
-      stop_distance = 6.0
+      stop_distance = 0.0
       if self.dRelk < (self.frogpilot_planner.frogtfollow * v_ego + stop_distance) and v_ego > 2.0 and self.frogpilot_planner.tracking_lead: # target 1.5s gap
         mtsc_active = True
         k_p = 0.1
