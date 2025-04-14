68e86b869bccdfc9230a72785040c6917e4449eb
mike8643 <nguyen95465@yahoo.com>
2025-04-13 23:08:10 -0400

    cem icon

--- selfdrive/ui/qt/onroad/annotated_camera.cc
+++ selfdrive/ui/qt/onroad/annotated_camera.cc
@@ -1166,7 +1166,7 @@ void AnnotatedCameraWidget::drawCEMStatus(QPainter &p) {
   }
 
   cemIconPosition.rx() = dmIconPosition.x();
-  cemIconPosition.ry() = dmIconPosition.y() - img_size / 2;
+  cemIconPosition.ry() = height() - 126 - img_size / 2;
   cemIconPosition.rx() += (rightHandDM ? -img_size : img_size) / (mapOpen ? 1.25 : 1);
 
   QRect cemWidget(cemIconPosition.x(), cemIconPosition.y(), img_size, img_size);
