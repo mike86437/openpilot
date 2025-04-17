279812b958ed693aa91aad5e3c2159131e5f316e
mike8643 <98910897+mike8643@users.noreply.github.com>
2025-04-17 12:12:02 -0400

    Update radard.py

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -251,6 +251,7 @@ def get_lead(v_ego: float, ready: bool, tracks: dict[int, Track], lead_msg: capn
       x = np.arange(len(y)) * DT_MDL
       drel_slope = np.polyfit(x, y, 1)[0]
       calc_vLead = np.clip(v_ego - drel_slope, 0, 40)
+      print(f"calc_vLead: {calc_vLead:.2f}, vLead: {lead_dict['vLead']:.2f}")
       lead_dict['vLead'] = (calc_vLead + float(lead_dict['vLead'])) / 2
   else:
     if hasattr(get_lead, "dRelk_hist"):
