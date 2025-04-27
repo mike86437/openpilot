812bd44d4f03f7469fa9bfcb01f60643c475e4ae
mike8643 <nguyen95465@yahoo.com>
2025-04-26 20:08:07 -0400

    log.Desire.none

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -56,9 +56,7 @@ class DesireHelper:
     one_blinker = carstate.leftBlinker != carstate.rightBlinker
     below_lane_change_speed = v_ego < frogpilot_toggles.minimum_lane_change_speed
     # Cancel nudgeless lane change in any state under these conditions:
-    # - Brake is pressed
     # - Driver is applying opposing steering torque.
-    cancel_lane_change = (carstate.brakePressed and frogpilot_toggles.nudgeless)
     if self.lane_change_direction == LaneChangeDirection.left  and (carstate.steeringPressed and carstate.steeringTorque < 0):
       cancel_lane_change = frogpilot_toggles.nudgeless  # openpilot applying CCW torque but driver is applying CW torque
     else if self.lane_change_direction == LaneChangeDirection.right and (carstate.steeringPressed and carstate.steeringTorque > 0):
@@ -66,6 +64,7 @@ class DesireHelper:
     if cancel_lane_change:
       self.lane_change_state = LaneChangeState.off
       self.lane_change_direction = LaneChangeDirection.none
+      self.desire = log.Desire.none
 
     if not (frogpilot_toggles.lane_detection and one_blinker) or below_lane_change_speed:
       lane_available = True
