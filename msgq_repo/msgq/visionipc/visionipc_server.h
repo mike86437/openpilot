4ac2895c6ef268a884956da6ec40712c2c7a331e
mike8643 <nguyen95465@yahoo.com>
2025-04-27 23:11:31 -0400

    revert

--- msgq_repo/msgq/visionipc/visionipc_server.h
+++ msgq_repo/msgq/visionipc/visionipc_server.h
@@ -1,31 +1,42 @@
 #pragma once
-
-#include <set>
+#include <vector>
 #include <string>
+#include <thread>
+#include <atomic>
+#include <map>
 
 #include "msgq/ipc.h"
 #include "msgq/visionipc/visionbuf.h"
 
+std::string get_endpoint_name(std::string name, VisionStreamType type);
+std::string get_ipc_path(const std::string &name);
 
-class VisionIpcClient {
-private:
-  std::string name;
-  Context * msg_ctx;
-  SubSocket * sock;
-  Poller * poller;
-
+class VisionIpcServer {
+ private:
   cl_device_id device_id = nullptr;
   cl_context ctx = nullptr;
+  uint64_t server_id;
 
-public:
-  bool connected = false;
-  VisionStreamType type;
-  int num_buffers = 0;
-  VisionBuf buffers[VISIONIPC_MAX_FDS];
-  VisionIpcClient(std::string name, VisionStreamType type, bool conflate, cl_device_id device_id=nullptr, cl_context ctx=nullptr);
-  ~VisionIpcClient();
-  VisionBuf * recv(VisionIpcBufExtra * extra=nullptr, const int timeout_ms=100);
-  bool connect(bool blocking=true);
-  bool is_connected() { return connected; }
-  static std::set<VisionStreamType> getAvailableStreams(const std::string &name, bool blocking = true);
+  std::atomic<bool> should_exit = false;
+  std::string name;
+  std::thread listener_thread;
+
+  std::map<VisionStreamType, std::atomic<size_t> > cur_idx;
+  std::map<VisionStreamType, std::vector<VisionBuf*> > buffers;
+
+  Context * msg_ctx;
+  std::map<VisionStreamType, PubSocket*> sockets;
+
+  void listener(void);
+
+ public:
+  VisionIpcServer(std::string name, cl_device_id device_id=nullptr, cl_context ctx=nullptr);
+  ~VisionIpcServer();
+
+  VisionBuf * get_buffer(VisionStreamType type);
+
+  void create_buffers(VisionStreamType type, size_t num_buffers, bool rgb, size_t width, size_t height);
+  void create_buffers_with_sizes(VisionStreamType type, size_t num_buffers, bool rgb, size_t width, size_t height, size_t size, size_t stride, size_t uv_offset);
+  void send(VisionBuf * buf, VisionIpcBufExtra * extra, bool sync=true);
+  void start_listener();
 };
