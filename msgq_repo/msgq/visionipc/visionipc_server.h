1cc83517b236ddc6104b2cd9ffb703ee923c5e5a
mike8643 <nguyen95465@yahoo.com>
2025-04-27 22:59:05 -0400

    revert

--- msgq_repo/msgq/visionipc/visionipc_server.h
+++ msgq_repo/msgq/visionipc/visionipc_server.h
@@ -1,49 +1,31 @@
 #pragma once
-#include <vector>
+
+#include <set>
 #include <string>
-#include <thread>
-#include <atomic>
-#include <map>
 
 #include "msgq/ipc.h"
 #include "msgq/visionipc/visionbuf.h"
 
-std::string get_endpoint_name(std::string name, VisionStreamType type);
-std::string get_ipc_path(const std::string &name);
 
-class VisionIpcServer {
+class VisionIpcClient {
 private:
+  std::string name;
+  Context * msg_ctx;
+  SubSocket * sock;
+  Poller * poller;
+
   cl_device_id device_id = nullptr;
   cl_context ctx = nullptr;
-  uint64_t server_id;
-
-  std::atomic<bool> should_exit = false;
-  std::string name;
-  std::thread listener_thread;
-
-  std::map<VisionStreamType, std::atomic<size_t> > cur_idx;
-  std::map<VisionStreamType, std::vector<VisionBuf*> > buffers;
-
-  Context * msg_ctx;
-  std::map<VisionStreamType, PubSocket*> sockets;
-
-  void listener(void);
-
-  // New member variable for horizontal shift
-  int horizontal_shift_amount_;
 
 public:
-  VisionIpcServer(std::string name, cl_device_id device_id = nullptr, cl_context ctx = nullptr);
-  ~VisionIpcServer();
-
-  VisionBuf * get_buffer(VisionStreamType type);
-
-  void create_buffers(VisionStreamType type, size_t num_buffers, bool rgb, size_t width, size_t height);
-  void create_buffers_with_sizes(VisionStreamType type, size_t num_buffers, bool rgb, size_t width, size_t height, size_t size, size_t stride, size_t uv_offset);
-  void send(VisionBuf * buf, VisionIpcBufExtra * extra, bool sync = true);
-  void start_listener();
-
-  // Setter function for horizontal shift
-  void set_horizontal_shift(int shift) { horizontal_shift_amount_ = shift; }
-  int get_horizontal_shift() const { return horizontal_shift_amount_; }
+  bool connected = false;
+  VisionStreamType type;
+  int num_buffers = 0;
+  VisionBuf buffers[VISIONIPC_MAX_FDS];
+  VisionIpcClient(std::string name, VisionStreamType type, bool conflate, cl_device_id device_id=nullptr, cl_context ctx=nullptr);
+  ~VisionIpcClient();
+  VisionBuf * recv(VisionIpcBufExtra * extra=nullptr, const int timeout_ms=100);
+  bool connect(bool blocking=true);
+  bool is_connected() { return connected; }
+  static std::set<VisionStreamType> getAvailableStreams(const std::string &name, bool blocking = true);
 };
