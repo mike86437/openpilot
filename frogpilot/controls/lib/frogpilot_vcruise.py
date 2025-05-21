2b773a44f49c1831146bc2ea39388601fa8ffa45
mike8643 <nguyen95465@yahoo.com>
2025-05-21 19:57:44 -0400

    disable float under

--- frogpilot/controls/lib/frogpilot_vcruise.py
+++ frogpilot/controls/lib/frogpilot_vcruise.py
@@ -102,8 +102,8 @@ class FrogPilotVCruise:
       v_cruise = min(v_ego - 0.25, v_cruise + 4.4704)
 
     # Float 5 mph under vcruise
-    if self.vtsc_target >= v_cruise and self.mtsc_target >= v_cruise and v_ego < (v_cruise - 0.5):
-      v_cruise = max(v_ego + 0.5, v_cruise - 2.2352)
+    # if self.vtsc_target >= v_cruise and self.mtsc_target >= v_cruise and v_ego < (v_cruise - 0.5):
+    #   v_cruise = max(v_ego + 0.5, v_cruise - 2.2352)
 
     if sm["carState"].standstill and not self.override_force_stop and sm["controlsState"].enabled and frogpilot_toggles.force_standstill and not (self.frogpilot_planner.tracking_lead and getattr(self.frogpilot_planner.lead_one, "dRel", float("inf")) < 15):
       self.forcing_stop = True
