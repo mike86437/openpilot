763cc72737c0ef51d46d109cd7f1e86c15bed542
mike8643 <nguyen95465@yahoo.com>
2025-05-29 08:59:20 -0400

    tFollow = self.frogpilot_planner.frogpilot_following.t_follow

--- frogpilot/controls/lib/frogpilot_vcruise.py
+++ frogpilot/controls/lib/frogpilot_vcruise.py
@@ -62,7 +62,7 @@ class FrogPilotVCruise:
       d_rel = lead.dRel
       v_lead = lead.vLead
       v_rel = v_ego - v_lead
-      tFollow = sm["frogpilotPlan"].tFollow
+      tFollow = self.frogpilot_planner.frogpilot_following.t_follow
       if (v_lead + min(d_rel / max(v_ego, 1e-3), 3)) < v_ego > CRUISING_SPEED and self.frogpilot_planner.tracking_lead:
         mtsc_active = True
         decelRate = (v_rel ** 2) / (2 * max(d_rel - v_lead * tFollow, 1e-6))
