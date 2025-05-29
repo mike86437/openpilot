663355201aa49acd3b05ff4f44a23afc0344eafc
mike8643 <98910897+mike8643@users.noreply.github.com>
2025-05-29 10:17:25 -0400

    Update frogpilot_vcruise.py

--- frogpilot/controls/lib/frogpilot_vcruise.py
+++ frogpilot/controls/lib/frogpilot_vcruise.py
@@ -101,7 +101,7 @@ class FrogPilotVCruise:
     # Float 10 mph over vcruise
     actuators = sm["carControl"].actuators
     if self.vtsc_target >= v_cruise and self.mtsc_target >= v_cruise and v_ego > (v_cruise + 0.25):
-      buffer = 0.0 if actuators.accel < 0 else 0.25
+      buffer = 0.0 if actuators.accel < 0.1 else 0.25
       v_cruise = min(v_ego - buffer, v_cruise + 4.4704)
 
     # Float 5 mph under vcruise
