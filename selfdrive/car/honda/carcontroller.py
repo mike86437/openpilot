dbe3c1475e63b2ae1c33911f1812d5a95abd53e7
mike8643 <nguyen95465@yahoo.com>
2025-06-28 13:10:07 -0400

    if self.CP.carFingerprint == (HONDA_CLARITY):

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -244,7 +244,10 @@ class CarController(CarControllerBase):
 
           if self.CP.enableGasInterceptor:
             # way too aggressive at low speed without this
-            gas_mult = interp(CS.out.vEgo, [0., 10.], [1.0, 1.0])
+            if self.CP.carFingerprint == (HONDA_CLARITY):
+              gas_mult = interp(CS.out.vEgo, [0., 10.], [1.0, 1.0])
+            else:
+              gas_mult = interp(CS.out.vEgo, [0., 10.], [0.4, 1.0])
             # send exactly zero if apply_gas is zero. Interceptor will send the max between read value and apply_gas.
             # This prevents unexpected pedal range rescaling
             # Sending non-zero gas when OP is not enabled will cause the PCM not to respond to throttle as expected
