075099be7965da93c333a579ecd9307be796a477
mike8643 <nguyen95465@yahoo.com>
2025-06-28 13:40:02 -0400

    back to if self.CP.carFingerprint == CAR.HONDA_CLARITY:

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -244,8 +244,7 @@ class CarController(CarControllerBase):
 
           if self.CP.enableGasInterceptor:
             # way too aggressive at low speed without this
-            if self.CP.carFingerprint == "HONDA_CLARITY":
-              print("Hello 2")
+            if self.CP.carFingerprint == CAR.HONDA_CLARITY:
               gas_mult = interp(CS.out.vEgo, [0., 10.], [1.0, 1.0])
             else:
               gas_mult = interp(CS.out.vEgo, [0., 10.], [0.4, 1.0])
