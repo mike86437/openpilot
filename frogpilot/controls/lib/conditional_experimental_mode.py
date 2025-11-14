78d3dc74b9fe8663e0123887876cd5eb5ebea9eb
mike8643 <nguyen95465@yahoo.com>
2025-11-14 14:26:35 -0500

    self.stop_light_detected and v_ego < 22.352

--- frogpilot/controls/lib/conditional_experimental_mode.py
+++ frogpilot/controls/lib/conditional_experimental_mode.py
@@ -92,7 +92,7 @@ class ConditionalExperimentalMode:
       model_stopping = self.frogpilot_planner.model_length < v_ego * model_time
 
       self.stop_light_filter.update(self.frogpilot_planner.model_stopped or model_stopping)
-      self.stop_light_detected = self.stop_light_filter.x >= THRESHOLD**2 and not self.frogpilot_planner.tracking_lead
+      self.stop_light_detected = self.stop_light_filter.x >= THRESHOLD**2 and not self.frogpilot_planner.tracking_lead and v_ego < 22.352
     else:
       self.stop_light_filter.x = 0
       self.stop_light_detected = False
